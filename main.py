497df092a8aa944f8d090491f8f44e1de1fd160f
aprico8888-cloud <aprico8888@gmail.com>
2026-04-25 10:43:06 +0900

    update main.py

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 scores = [80, 90, 70]
-print(sum(scores) / len(scores))
+
+avg = sum(scores)/len(scores)
+
+print("平均:", avg)
+print("最大:", max(scores))
+
+for s in scores:
+    if s >= avg:
+        print("平均以上:", s)
