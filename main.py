bffc6630ca7d7bb077b1432105d85b9135156634
aprico8888-cloud <aprico8888@gmail.com>
2026-04-25 21:15:25 +0900

    ゼロ情Ⅰ①

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-scores = [80, 90, 70]
-
-# 平均
-print(sum(scores)/len(scores))
-
-# 最大値
-print(max(scores))
-
-# 60以上だけ表示
-for s in scores:
-    if s >= 60:
-        print(s)
+print('Hello, World!')
